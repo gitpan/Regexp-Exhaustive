c38a11188d4217b61abc8a8326498ec64f0261c2
Johan Lodin <lodin@cpan.org>
2008-01-06 06:03:45 +0000

    Import of LODIN/Regexp-Exhaustive-0.04 from CPAN.
    gitpan-cpan-distribution: Regexp-Exhaustive
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         LODIN/Regexp-Exhaustive-0.04.tar.gz
    gitpan-cpan-author:       LODIN
    gitpan-cpan-maturity:     released

--- lib/Regexp/Exhaustive.pm
+++ lib/Regexp/Exhaustive.pm
@@ -1,7 +1,7 @@
 package Regexp::Exhaustive;
 use 5.006001;
 
-$VERSION = 0.03;
+$VERSION = 0.04;
 
 use base 'Exporter';
 @EXPORT_OK = qw/ exhaustive /;
@@ -77,6 +77,7 @@ sub exhaustive {
                 $save_match = qr/$pattern/;
             }
             else {
+                no warnings 'closure';
                 $save_match = qr/
                     (?{
                         $Vars ||= $#+
@@ -188,7 +189,7 @@ It works a lot like the familiar C<m//g> regarding return values.
 
 Beware that exhaustive matching may generate a very large number of matches. If you only need overlapping matches that's easily achieved. Overlapping matching has a maximum number of matches being the length of the string plus one.
 
-This is an initial release, and many things may change for the next version. If you feel something is missing or poorly designed, now is the time to voice your opinion.
+This is an initial release, and some things may change for the next version. If you feel something is missing or poorly designed, now is the time to voice your opinion.
 
 
 =head1 EXPORTED FUNCTIONS
@@ -197,7 +198,7 @@ Nothing is exported by default. The C<:ALL> tag exports everything that can be e
 
 =over
 
-=item exhaustive(STRING => qr/PATTERN/)
+=item C<< exhaustive(STRING => qr/PATTERN/) >>
 
 Exhaustively generates and returns all matches in list context. Returns the number of matches in scalar context.
 
@@ -207,9 +208,9 @@ This is like C<m//g> in list context, except C<m//g> returns all capturing subpa
 
 This method does not interact with C<pos($str)> and can be safely intermixed with other match operations against the string.
 
-Worthy to note is that it's much more efficient to use C<qr/(.)/> than C<qr/./>, i.e. making direct use of C<$1> instead of having C<&exhaustive> calculate C<$&>.
+Worthy to note is that it's much more efficient to use C<qr/(.)/> than C<qr/./>, i.e. making direct use of C<$1> instead of having C<exhaustive> calculate C<$&>.
 
-=item exhaustive(STRING => qr/PATTERN/, qw[ $1 $2 @- $^R ... ])
+=item C<< exhaustive(STRING => qr/PATTERN/, qw[ $1 $2 @- $^R ... ]) >>
 
 Optionally, you can specify which variables to return. Arrays and hashes will end up as references in the return list.
 
@@ -234,15 +235,15 @@ Supported variables:
     $'              $POSTMATCH
     $+              $LAST_PAREN_MATCH
     $^N
-    @+              @LAST_MATCH_END
     @-              @LAST_MATCH_START
+    @+              @LAST_MATCH_END
     %-
     %+
     $^R             $LAST_REGEXP_CODE_RESULT
 
-Using C<$`>, C<$&>, and C<$'> with C<&exhaustive> doesn't impose the performance penalty that those variables otherwise impose. However, they're usually slower to use than rewriting the pattern to use capturing subpatterns. In particular, C<$&> is often easy to avoid by just adding an outer capturing parenthesis and using C<$1> instead.
+Using C<$`>, C<$&>, and C<$'> with C<exhaustive> doesn't impose the performance penalty that those variables otherwise impose. However, they're usually slower to use than rewriting the pattern to use capturing subpatterns. In particular, C<$&> is often easy to avoid by just adding an outer capturing parenthesis and using C<$1> instead.
 
-=item overlapping(STRING => qr/PATTERN/)
+=item C<< overlapping(STRING => qr/PATTERN/) >>
 
 If this function would exist, but it doesn't, it would be like global match but try to match from every position of the string. It's like an exhaustive match that doesn't backtrack inside the pattern.
 
@@ -250,7 +251,7 @@ This function doesn't exist because it isn't needed. Instead use
 
     exhaustive(STRING => qr/(?>PATTERN)/)
 
-i.e. wrap the pattern with the C<< (?>...) >> assertion. This will lock the match once the pattern has matched, forcing the regex engine to skip behind the pattern when backtracking, thus moving forward on string for the next match.
+i.e. wrap the pattern with the C<< (?>...) >> assertion. This will lock the match once the pattern has matched, forcing the regex engine to skip behind the pattern when backtracking, thus moving forward on the string for the next match.
 
 Using
 
@@ -276,7 +277,7 @@ This will loop forever.
 
 =item Use of uninitialized value in &Regexp::Exhaustive::exhaustive
 
-(W|uninitialized) The string given to &exhaustive was not defined.
+(W|uninitialized) The string given to C<exhaustive> was not defined.
 
 =item The second argument to &Regexp::Exhaustive::exhaustive must be a Regexp object (qr//)
 
@@ -288,7 +289,7 @@ This will loop forever.
 
 =item Bad variable name(s) to &Regexp::Exhaustive::exhaustive: %s
 
-(F) You supplied a string that didn't look like a known regex variable. See C<&exhaustive> for supported variables.
+(F) You supplied a string that didn't look like a known regex variable. See C<exhaustive> for supported variables.
 
 =back
 
@@ -371,14 +372,14 @@ L<Set::CrossProduct|Set::CrossProduct> gives you the cross product of a set, and
 
 =head2 N-ary count
 
-Using C<Regexp::Exhaustive> you can generate all values of a certain digit length using an n-ary count. This is demonstrated for binary numbers with a length of three.
+Using C<Regexp::Exhaustive> you can generate all values of a certain digit length using an n-ary count. This is demonstrated for binary numbers with a length of three. (The length is assumed to be greater than one.)
 
     use Regexp::Exhaustive 'exhaustive';
 
     sub all_values {
         my ($n, @base) = @_;
         my $str = (join('', @base) . "\n") x $n;
-        my $re = ".*?(.).*\n" x $n;
+        my $re = qr/.*?(.).*\n/ x $n;
         return map { join '', @$_ } exhaustive($str, qr/^$re/);
     }
 
